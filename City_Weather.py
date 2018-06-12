e137c946161a6813e488fc6138b1c187f2903aca
zhhiyuan <31727698+zhhiyuan@users.noreply.github.com>
2018-06-12 20:52:03 +0800

    更改了查询天气代码
    2018年6月12日20:51:58

--- City_Weather.py
+++ City_Weather.py
@@ -1,31 +1,26 @@
-#coding=utf-8
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
-from city import city
-def search_weather(cityname):
+import json, sys, requests
+
+# 输入地点
+def search_weather(city):
+    # 下载天气JSON
+    weatherJsonUrl = "http://wthrcdn.etouch.cn/weather_mini?city=%s" % (city)
+    response = requests.get(weatherJsonUrl)
     try:
-        citycode = city[cityname]
-        try:
-            resp = urlopen('http://www.weather.com.cn/weather/' + citycode + '.shtml')
-            soup = BeautifulSoup(resp, 'html.parser')
-            tagToday = soup.find('p', class_="tem")  # 第一个包含class="tem"的p标签即为存放今天天气数据的标签
-            try:
-                temperatureHigh = tagToday.span.string  # 有时候这个最高温度是不显示的，此时利用第二天的最高温度代替。
-            except AttributeError as e:
-                temperatureHigh = tagToday.find_next('p', class_="tem").span.string  # 获取第二天的最高温度代替
-
-            temperatureLow = tagToday.i.string  # 获取最低温度
-            weather = soup.find('p', class_="wea").string  # 获取天气
-
-            return (cityname + ':\n' +'最低温度:' + temperatureLow + '\n' + '最高温度:' + temperatureHigh + '\n' + '天气:' + weather)
-        except:
-            return ('系统掉线了呢。。。')
+        response.raise_for_status()
+        # 将json文件格式导入成python的格式
+        weatherData = json.loads(response.text)
+        w = weatherData['data']
+        date_a = (w['forecast'][0]['date'])  # 日期
+        highTemp = (w['forecast'][0]['high'])  # 最高温度
+        lowTemp = (w['forecast'][0]['low'])  # 最低温度
+        weather = (w['forecast'][0]['type'])  # 天气
+        str = ''
+        str = str + ("温度：最" + lowTemp + '，最' + highTemp + '\n') + ("天气：" + weather) + (
+        "\n今日着装：" + w['ganmao']) + ("\n当前温度：" + w['wendu'] + "℃")
+        return str
     except:
-        return ("中国找不到这个城市。。。")
+        return ("网址请求出错")
 
-#2018年4月22日17:17:10
-if __name__ =='__main__':
-    # cityname为存放整理后的文件夹路径
-    # 结果会以字符串形式返回
-    cityname=input('请输入你想查询天气的城市：')
-    print(search_weather(cityname))
+if __name__ == '__main__':
+    city = input("请输入天气地点：")
+    print(search_weather(city))
--- Time.py
+++
@@ -1,7 +0,0 @@
-from time import strftime,gmtime
-def get_time():
-    now_time=strftime("%Y-%m-%d %H:%M:%S",gmtime())
-    return ('当前时间是'+now_time)
-
-if __name__=='__main__':
-    print(get_time())
